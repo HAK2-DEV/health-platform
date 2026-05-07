2a65e3c2bcba2f876e6e867a1ddd5f1c519d948b
HAK2 <00jonghak@gmail.com>
2026-05-07 23:17:27 +0900

    javascrpt 연습, let, const, alert

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("script.js 연결 성공!")
+
+let userName = "HAK2"
+let userAge = 30
+
+console.log("내 이름:", userName)
+console.log("내 나이:", userAge)
+
+alert("안녕, " +  userName + " !" + " 내 나이는 " + userAge +"야.")
+alert("다음에 보자!")
